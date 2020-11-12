a2731cf5f45487634afe01b87127e81e2d16a42e
Rachman Aldiansyah <rachmanaldiansyah03@gmail.com>
2020-11-12 10:58:09 +0700

    Upload Project

---
+++ src/main/java/if1/latihan46/tandanyakamu/Tester.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.latihan46.tandanyakamu;
+
+/**
+ *
+ * @author
+ * NAMA     : Rachman Aldiansyah
+ * KELAS    : IF-1
+ * NIM      : 10119038
+ * Deskripsi Program : Program ini berisi menghitung usia dengan pendekatan object oriented
+ */
+
+import java.util.Scanner;
+
+public class Tester {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Age usia = new Age(2020);
+        
+        System.out.print("Masukan Tahun Lahir Anda : ");
+        usia.setYearBirth(sc.nextInt());
+        
+        
+        System.out.println("\n=====Hasil Perhitungan Umur=====");
+        System.out.println("Tahun lahir anda : " + usia.getYearBirth());
+        System.out.println("Hari ini Tahun : " + usia.getYearNow());
+        System.out.println("Umur kamu sampai hari ini adalah " + usia.hitungUmur() + " tahun");
+        System.out.println("Tandanya Kamu ".concat(usia.tandanyaKamu(usia.hitungUmur())));
+        
+        
+    }
+}
